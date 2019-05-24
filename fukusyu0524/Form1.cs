d56af53190d332ac1d70c135c9e72b7fa7e82ae2
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-05-24 13:29:02 +0900

    a

--- fukusyu0524/Form1.cs
+++ fukusyu0524/Form1.cs
@@ -10,11 +10,59 @@ using System.Windows.Forms;
 
 namespace fukusyu0524
 {
+    
     public partial class Form1 : Form
     {
+    int a,b;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += a;
+            label1.Top += b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = 1;
+            b = 0;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = -1;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            a = -1;
+            b = 0;
         }
     }
 }
